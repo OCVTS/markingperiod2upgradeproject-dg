dfd4e455a93029073e12ae93b6995ac11c072b9f
MM38162 <38162@ocvts.org>
2025-01-25 06:29:19 +0000

    fthfht

--- script.js
+++ script.js
@@ -1,64 +1,46 @@
-/*setting all variables to 0 so it starts off as a fresh game. The playerRoll and computerRoll are to indicate whos rolling what. The playerTotal and computerTotal are the combined scores of the numbers they rolled. 
-The playerScore and computerScore are to keep track of who won how many rounds.  */
-let playerRoll1 = 0;
-let computerRoll1 = 0;
-let playerRoll2 = 0;
-let computerRoll2 = 0;
-let playerTotal = 0;
-let computerTotal = 0;
-let playerScore = 0;
-let computerScore = 0;
+//Variables for my dice game    
+    let player = "Player";
+    let computer = "Computer";
+  
 
-//by clicking the button it triggers the button to "roll" the dice.//
-document.getElementById('rollButton').addEventListener('click', rollDice);
+    // Function to roll the dice, get a number then the image associated with that number
+    function rollTheDice() {
+        setTimeout(function () {
+             randomNumber1 = Math.floor(Math.random() * 6) + 1;
+             randomNumber3 = Math.floor(Math.random() * 6) + 1;
 
+             randomNumber2 = Math.floor(Math.random() * 6) + 1;
+             randomNumber4 = Math.floor(Math.random() * 6) + 1;
 
+//the images that come with the random selected number, so if a 6 is pulled dice image 6 is displayed
+            document.querySelector(".img1").setAttribute("src",
+                "dice" + randomNumber1 + ".png");
 
-/*********** function to generate random dice rolls ***************/
-function rollDice() {
-    playerRoll1 = Math.floor(Math.random() * 6) + 1;
-    playerRoll2 = Math.floor(Math.random() * 6) + 1;
-    computerRoll1 = Math.floor(Math.random() * 6) +1;
-    computerRoll2 = Math.floor(Math.random() * 6) +1;
-    calculateScore();
-}
+            document.querySelector(".img2").setAttribute("src",
+                "dice" + randomNumber2 + ".png");
 
+                document.querySelector(".img3").setAttribute("src",
+                    "dice" + randomNumber3 + ".png");
 
-/*********** function to calculate the result, determine winner, and update the score ***************/
-function calculateScore() {
-playerTotal = playerRoll1 + playerRoll2;
-computerTotal = computerRoll1 + computerRoll2;
+                    document.querySelector(".img4").setAttribute("src",
+                        "dice" + randomNumber4 + ".png");
 
-// This is how we will determine the winner of the game//
-if (playerTotal > computerTotal) {
-    document.getElementById("winner").innerHTML = 'Player wins!';
-    playerScore += 1;
+                        //statements declaring who won
+            if (randomNumber1+randomNumber3 === randomNumber2+randomNumber4) {
+                document.querySelector("h1").innerHTML = "Draw!";
+            }
 
-}else if(playerTotal < computerTotal) {
-    document.getElementById("winner").innerHTML = 'Computer wins!';
-    computerScore += 1;
+            else if (randomNumber1+randomNumber3 < randomNumber2+randomNumber4) {
+                document.querySelector("h1").innerHTML
+                                = (computer + " Wins!");
 
-}else if(playerTotal === computerTotal) {
-    document.getElementById("winner").innerHTML = `It's a tie!`;
+            }
 
-}else {
-    document.getElementById("winner").innerHTML = 'There is an error!';
-}
+            else {
+                document.querySelector("h1").innerHTML
+                                = (player + " Wins!");
 
-displayResults()
-}
-
-
-
-/*********** function to display the current roll and scores ***************/
-function displayResults() {
-    document.getElementById("playerDice").innerHTML = `Player rolled a ${playerRoll1} and a ${playerRoll2}`;
-    document.getElementById("computerDice").innerHTML = `Computer rolled a ${computerRoll1} and a ${computerRoll2}`;
-    document.getElementById("playerTotal").innerHTML = `Player total roll: ${playerTotal}`;
-    document.getElementById("computerTotal").innerHTML = `Computer total roll: ${computerTotal}`;
-    document.getElementById("playerScore").innerHTML = `Player score: ${playerScore}`;
-    document.getElementById("computerScore").innerHTML = `Computer score: ${computerScore}`;
-
-}
-
+            }
+        }, 2500);
+    }
 
